2aec00f5538c0ffc3dfafdd50db9a73c6ef83180
Anton <scoutrul@mail.ru>
2018-09-25 22:31:37 +0300

    добавление и изменение тасков

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,69 +1,56 @@
 import React, { Component } from 'react';
 import { Table, Input, Icon } from 'antd';
-import { map } from 'lodash';
+import { map, reverse } from 'lodash';
 import { database } from '../../firebase';
 
-const columns = [{
-  title: 'Задачи',
-  dataIndex: 'title',
-  key: 'title',
-  render: (text, item) => {
-    const showText = item.done ? <strike>{text}</strike> : <span>{text}</span>;
-    const control = item.done ? <button><Icon type="check" /></button> : <button><Icon type="clock-circle" /></button>;
-
-    return (
-        <div> 
-            {showText} {control}
-        </div>);
-    },
-}];
-
-const data = [
-    {
-        key: '1',
-        title: 'Добавить компонент в разметку',
-        done: true,
-    }, {
-        key: '2',
-        title: 'Вывести таблицу',
-        done: true,
-    }, {
-        key: '3',
-        title: 'Интегрироваться с firebase',
-        done: true,
-    }, {
-        key: '4',
-        title: 'Изменение, удаление тасков',
-        done: false,
-    }
-];
-
-
-
 class TodoList extends Component{
-
     state = {
-        addValue: 'Новая задача',
-        data: null,
+        addValue: null,
+        data: [],
     }
 
     componentDidMount() {
+       this.getData();
+    }
+
+    getData = () => {
         database.ref('task').on('value', (snapshot) => {
-            this.formatData(snapshot.val())
+            let data = [];
+            map(snapshot.val(), (val, key) => {
+                data.push({
+                    key,
+                    title: val.title,
+                    done: val.done,
+                })
+            }); 
+
+            let reversedata = reverse(data);
+    
+            this.setState({ data: reversedata })
         })
     }
 
-    formatData = (snapshot) => {
-        const container = [];
-        map(snapshot, (val, key) => {
-            container.push({
-                key,
-                title: val.title,
-                done: val.done,
-            })
-        }); 
+    getColumns = () => {
+        return [{
+            title: '',
+            dataIndex: 'title',
+            key: 'title',
+            render: (text, item) => {
+                const showText = item.done ? <strike>{text}</strike> : <span>{text}</span>;
+                const control = item.done ? <button onClick={() => this.checkThis(item, false)}><Icon type="check" /></button> : <button onClick={() => this.checkThis(item, true)}><Icon type="clock-circle" /></button>;
+            
+                return (
+                    <div> 
+                        {control} {showText}
+                    </div>
+                );
+            },
+      }]
+    };
 
-        this.setState({ data: container })
+    checkThis = (item, bool) => {
+        database.ref(`task/${item.key}`).update({ ...item, done: bool });
+        this.getData();
     }
 
     addTask = () => {
@@ -76,21 +63,26 @@ class TodoList extends Component{
         })
     }
 
+    addTaskForm = () => (
+        <Input 
+            value={this.state.addValue} 
+            onChange={(val) => this.valueChange(val)} 
+            addonAfter={
+                <button onClick={() => this.addTask()}>
+                    <Icon type="plus" />
+                </button>
+            } 
+            placeholder="Новая задача" 
+        />
+    )
+
     render(){
         return(
             <div>
-                <Input 
-                    value={this.state.addValue} 
-                    onChange={(val) => this.valueChange(val)} 
-                    addonAfter={
-                        <button onClick={() => this.addTask()}>
-                            <Icon type="plus" />
-                        </button>
-                    } 
-                    placeholder="Новая задача"
-                />
+                <h2>Задачи</h2>
+                {this.addTaskForm()}
                 <Table 
-                    columns={columns} 
+                    columns={this.getColumns()} 
                     dataSource={this.state.data} 
                     pagination={false} 
                 />
